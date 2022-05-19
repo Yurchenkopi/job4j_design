7c081eed0358735ad81754f1e097a205ee7907cb
Pavel <heppie@yandex.ru>
2022-05-19 21:00:27 +0300

    Revert "0.3. BufferedReader. [#252489 #302659]"
    This reverts commit c1effa34f43bb30398447fa64d2a0fd14fd7c1a6.

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -5,18 +5,24 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class LogFilter {
     public List<String> filter(String file) {
         List<String> data = new ArrayList<>();
-        Pattern pattern = Pattern.compile(".*\\s" + "404" + "\\s.[^\\s]*$");
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            data = in.lines()
-                    .filter(s -> pattern.matcher(s).find())
+           data = in.lines()
+                    .map(l -> l.split(" "))
+                    .filter(f -> Integer.parseInt(f[f.length - 2]) == 404)
+                    .map(s -> {
+                        StringBuilder b = new StringBuilder();
+                        for (String str : s) {
+                            b.append(str);
+                            b.append(' ');
+                        }
+                        return b + System.lineSeparator();
+                    })
                     .collect(Collectors.toList());
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -26,6 +32,7 @@ public class LogFilter {
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        log.forEach(System.out :: println);
+        System.out.println(log);
+
     }
 }
