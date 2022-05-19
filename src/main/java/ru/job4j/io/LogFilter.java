75ea50b42de4df0b81013e6e6bf361d50a3e193f
Pavel <heppie@yandex.ru>
2022-05-19 21:28:49 +0300

    0.4. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,38 +1,43 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class LogFilter {
     public List<String> filter(String file) {
         List<String> data = new ArrayList<>();
+        Pattern pattern = Pattern.compile(".*\\s" + "404" + "\\s.[^\\s]*$");
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-           data = in.lines()
-                    .map(l -> l.split(" "))
-                    .filter(f -> Integer.parseInt(f[f.length - 2]) == 404)
-                    .map(s -> {
-                        StringBuilder b = new StringBuilder();
-                        for (String str : s) {
-                            b.append(str);
-                            b.append(' ');
-                        }
-                        return b + System.lineSeparator();
-                    })
+            data = in.lines()
+                    .filter(s -> pattern.matcher(s).find())
                     .collect(Collectors.toList());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         return data;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String s : log) {
+                out.printf("%s%n", s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        System.out.println(log);
-
+        log.forEach(System.out :: println);
+        save(log, "404.txt");
     }
 }
