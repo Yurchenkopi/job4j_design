cb8f044656cc6bbd57308e059445a676fa211d6a
Pavel <heppie@yandex.ru>
2022-05-21 20:39:15 +0300

    2. Анализ доступности сервера. [#859 #303567]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -5,27 +5,21 @@ import java.util.StringJoiner;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             boolean startRec = false;
-            StringJoiner sj = new StringJoiner("");
             for (String s = in.readLine(); s != null; s = in.readLine()) {
                 String[] str = s.split("\\s");
                 int status = Integer.parseInt(str[0]);
                 if ((!startRec && (status == 400 || status == 500))
                         || (startRec && (status == 200 || status == 300))) {
-                    sj.add(str[1]);
-                    sj.add(";");
+                    out.print(str[1]);
+                    out.print(";");
                     startRec = !startRec;
                     if (!startRec) {
-                        sj.add(System.lineSeparator());
+                        out.println();
                     }
                 }
-            }
-            String rsl = sj.toString();
-            try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-                out.print(rsl);
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         } catch (IOException e) {
             e.printStackTrace();
