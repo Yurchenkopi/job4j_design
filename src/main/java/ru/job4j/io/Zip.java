ebcbd0bf996449179a1f13b2a0dad8db77156d42
Pavel <heppie@yandex.ru>
2022-05-25 00:23:30 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -8,8 +10,15 @@ import java.util.zip.ZipOutputStream;
 public class Zip {
 
     public void packFiles(List<File> sources, File target) {
-        for (File f : sources) {
-            packSingleFile(f, target);
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File f : sources) {
+                zip.putNextEntry(new ZipEntry(f.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(f))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -24,13 +33,38 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    private static void validate(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Source/Target/Searching parameter are null.");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        String source = argsName.get("d");
+        String filter = argsName.get("e");
+        Path path = Paths.get(source);
+        if (!path.toFile().exists()) {
+            throw new IllegalArgumentException("Source root doesn't exist");
+        }
+        if (!filter.startsWith(".")) {
+            throw new IllegalArgumentException("Incorrect searching parameter. It should be starts with '.' ");
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        validate(args);
+        ArgsName argsName = ArgsName.of(args);
+        String source = argsName.get("d");
+        String filter = argsName.get("e");
+        String target = argsName.get("o");
         Zip zip = new Zip();
-        List<File> list = List.of(
+        List<File> list = Search.search(Paths.get(source), p -> !p.toString().endsWith(filter)).stream()
+                .map(Path::toFile)
+                .toList();
+        List<File> testList = List.of(
                         new File("./pom.xml"),
                         new File("./result.txt"),
                         new File("./server.log")
         );
-        zip.packFiles(list, new File("./123.zip"));
+        zip.packFiles(list, new File(target));
     }
 }
