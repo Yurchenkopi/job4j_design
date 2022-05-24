c7b121c1c06137ac7750501aad61af1ce3b77aa1
Pavel <heppie@yandex.ru>
2022-05-24 19:26:07 +0300

    5. Валидация параметров запуска. [#246865 #304619]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -18,6 +18,17 @@ public class Search {
         if (args.length < 2) {
             throw new IllegalArgumentException("Root folder and/or Searching parameter are null. Usage java -jar search.jar ROOT_FOLDER, SEARCHING_PARAMETER");
         }
+        Path path = Paths.get(args[0]);
+        if (!path.toFile().exists()) {
+            throw new IllegalArgumentException("Root doesn't exist");
+        }
+        if (!path.toFile().isDirectory()) {
+            throw new IllegalArgumentException("Searching root is file");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Incorrect searching parameter. It should be starts with '.' ");
+        }
+
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
