631816ecdcec154aec1bfe4e80f0af1dec803b76
Pavel <heppie@yandex.ru>
2022-05-24 22:51:58 +0300

    5.2. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public void packFiles(List<File> sources, File target) {
+        for (File f : sources) {
+            packSingleFile(f, target);
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Zip zip = new Zip();
+        List<File> list = List.of(
+                        new File("./pom.xml"),
+                        new File("./result.txt"),
+                        new File("./server.log")
+        );
+        zip.packFiles(list, new File("./123.zip"));
+    }
+}
