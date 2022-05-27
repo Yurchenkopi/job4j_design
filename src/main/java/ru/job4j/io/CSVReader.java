763fad89cda8632cae756c7c544d225577e37eb3
Pavel <heppie@yandex.ru>
2022-05-28 02:06:20 +0300

    7. Scanner [#504791 #306166]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
-public class CSVReader {
+public class CSVReader  {
 
     public static void handle(ArgsName argsName) throws IOException {
         validate(argsName);
