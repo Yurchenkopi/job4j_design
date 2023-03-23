350ec4e410e4f9a3520167d325e69edabb6d559b
Yurchenkopi <heppie@yandex.ru>
2023-03-23 13:55:06 +0300

    1. String pool, Integer pool [#504931 #403510]

---
+++ src/main/java/ru/job4j/pool/StringPoolExample.java
@@ -0,0 +1,33 @@
+package ru.job4j.pool;
+
+public class StringPoolExample {
+    public static void main(String[] args) {
+        /*
+        String string1 = new String("Hello");
+        String string2 = new String("Hello");
+        String string3 = "Hello";
+        String string4 = "Hello";
+        System.out.println(string1 == string2);
+        System.out.println(string3 == string4);
+        System.out.println(string1 == string3);
+        System.out.println(string2 == string4);
+
+        String string1 = "Hello, world";
+        String string2 = "Hello, " + "world";
+        System.out.println(string1 == string2);
+
+        String string1 = "Hello, world";
+        String string2 = "Hello, ";
+        String string3 = string2 + "world";
+        System.out.println(string1 == string3);
+
+        String string1 = "Hello";
+        String string2 = new String("Hello");
+        String string3 = string2.intern();
+        System.out.println(string1 == string3);
+
+        System.out.println(new String("New string") == new String("New string"));
+        System.out.println(new String("Interned string").intern() == new String("Interned string").intern());
+         */
+    }
+}
