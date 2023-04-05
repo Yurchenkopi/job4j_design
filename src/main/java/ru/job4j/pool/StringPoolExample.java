709dd63b72438c2c99ad618e45866738f7da59ca
Yurchenkopi <heppie@yandex.ru>
2023-04-05 14:22:11 +0300

    0. Принцип единственной ответственности [#4913]

--- src/main/java/ru/job4j/pool/StringPoolExample.java
+++ src/main/java/ru/job4j/pool/StringPoolExample.java
@@ -2,8 +2,10 @@ package ru.job4j.pool;
 
 public class StringPoolExample {
     public static void main(String[] args) {
+
+        String string1 = new String("");
+        Integer test = new Integer(1333);
         /*
-        String string1 = new String("Hello");
         String string2 = new String("Hello");
         String string3 = "Hello";
         String string4 = "Hello";
@@ -28,6 +30,8 @@ public class StringPoolExample {
 
         System.out.println(new String("New string") == new String("New string"));
         System.out.println(new String("Interned string").intern() == new String("Interned string").intern());
+
          */
+
     }
 }
